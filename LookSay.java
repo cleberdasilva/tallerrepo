d8c1862cfcca112f5e9545a35cdb28f37a58c896
cleberdasilva <cleber.supertux@gmail.com>
2026-04-24 19:42:15 -0300

    Add LookSay class with countAndSay implementation

---
+++ LookSay.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class LookSay {
+    public static String nextTerm(String prevString){
+        StringBuilder current = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i < prevString.length(); i++){
+            if (prevString.charAt(i) == prevString.charAt(i - 1)){
+                count++;
+            } else {
+                current.append(count).append(prevString.charAt(i - 1));
+                count = 1;
+            }
+        }
+        current.append(count).append(prevString.charAt(prevString.length() - 1));
+
+        return current.toString();
+        }
+
+        public static String countAndSay(int n){
+            if (n == 1)
+                return "1";
+            String prevString = countAndSay(n - 1);
+
+            return nextTerm(prevString);
+        }
+
+        public static void main(String[] args){
+            int target = 911; //type to exit the loop
+            int n = 0;
+            Scanner scanner = new Scanner(System.in);
+            while (n != target) {
+                System.out.print("Enter n: ");
+                n = scanner.nextInt();
+                System.out.println(countAndSay(n));
+            }
+            scanner.close();
+        }
+    }   
